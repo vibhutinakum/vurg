c14e16dc795918137bd23348e7266b6b329a0f3a
Pitc@1597 <vibhuti@primaryitcare.com>
2021-07-01 16:15:31 +0530

    comming soon page deisng done

---
+++ assets/js/general.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function(e) {
+    var headerHeight = jQuery('header').height();
+    if ( jQuery(window).scrollTop() > headerHeight) {
+        jQuery('header').addClass("fixed-header");
+    }
+    jQuery(window).on('scroll', function(e) {
+        var scroll = jQuery(window).scrollTop();
+        if ( scroll > headerHeight) {
+            jQuery('header').addClass("fixed-header");
+        } else {
+            jQuery('header').removeClass("fixed-header");
+        }
+    });
+    jQuery(".mobile-menu").on('click', function(e) {
+        jQuery('body').toggleClass("sidebar-open");
+        jQuery(this).find('ul').toggleClass("active");
+    });
+});
